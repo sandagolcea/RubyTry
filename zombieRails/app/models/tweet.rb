e763a001754b720ffdc712c206e5680f564ca264
Sanda Golcea <sanda.golcea@gmail.com>
2014-10-19 23:06:52 -0700

    Modified the view segment (html.erb files)

--- zombieRails/app/models/tweet.rb
+++ zombieRails/app/models/tweet.rb
@@ -1,2 +1,21 @@
 class Tweet < ActiveRecord::Base
+  validates :status, presence: true, length: { minimum: 3 }
+  belongs_to :zombie
 end
+
+  #validates_presence_of :status
+  #validates_numericality_of :fingers # => number
+  #validates_uniqueness_of :toothmarks
+  #validates_confirmation_of :password # => matches 2 identical
+  #validates_acceptance_of :zombification # => checkbox
+  #validates_length_of :password, minimum: 3
+  #validates_format_of :email, with: /regex/i
+  #validates_inclusion_of :age, in: 21..99
+  #validates_exclusion_of :age, in: 0...21, message "Must be 21"
+  
+  #validates:
+  #presence: true
+  #uniqueness: true
+  #numericality: true
+  #length: { minumum: 0, maximum: 2000 }
+  #format: { with: /.*/ }
